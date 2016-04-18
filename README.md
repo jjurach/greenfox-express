126cffe6019e754ee62af31f58093e62a5f063c2
James Jurach <james.jurach@gmail.com>
2016-04-17 20:11:59 -0500

    initial

---
+++ README.md
@@ -0,0 +1,36 @@
+# Greenfox Express Project
+A simple Express.js web application with Stormpath features.
+
+### Get Started!
+
+1. Clone this repo to your computer, and cd into the project directory:
+
+  ```bash
+  git clone https://github.com/jjurach/greenfox-express.git
+  cd greenfox-express
+  ```
+
+2. Install the dependencies from package.json:
+
+  ```bash
+  npm install
+  ```
+
+3. Export your Stormpath API Key ID / Secret and Application HREF Environment Variables:
+
+  ```bash
+  export STORMPATH_CLIENT_APIKEY_ID=xxx
+  export STORMPATH_CLIENT_APIKEY_SECRET=xxx
+  export STORMPATH_APPLICATION_HREF=xxx
+  ```
+
+4. Start the server:
+
+  ```bash
+  node server.js
+  ```
+
+5. Visit [http://localhost:3000/](http://localhost:3000/) in your browser
+
+## Learn More
+Visit [http://docs.stormpath.com/nodejs/express/latest/](http://docs.stormpath.com/nodejs/express/latest/)
---
+++ server.js
@@ -0,0 +1,69 @@
+'use strict';
+
+var express = require('express');
+var stormpath = require('express-stormpath');
+var winston = require('winston');
+var expressWinston = require('express-winston');
+
+var routes = require('./lib/routes');
+
+/**
+ * Create the Express application.
+ */
+var app = express();
+
+/**
+ * Application settings.
+ */
+app.set('trust proxy',true);
+app.set('view engine', 'jade');
+app.set('views', './lib/views');
+app.locals.siteName = 'Greenfox Express Project';
+
+/**
+ * Logger initialization.
+ */
+
+var router = express.Router();
+
+router.use(expressWinston.logger({
+  transports: [
+    new winston.transports.Console({
+      json: true,
+      colorize: true,
+    }),
+  ],
+  meta: true,
+  expressFormat: true,
+  colorStatus: true,
+}));
+
+/**
+ * Stormpath initialization.
+ */
+
+console.log('Initializing Stormpath');
+
+app.use(stormpath.init(app, {
+  expand: {
+    customData: true
+  }
+}));
+
+/**
+ * Route initialization.
+ */
+app.use('/', routes);
+
+app.on('stormpath.ready',function () {
+  console.log('Stormpath Ready');
+});
+
+/**
+ * Start the web server.
+ */
+var port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Server listening on http://localhost:' + port);
+});
+
